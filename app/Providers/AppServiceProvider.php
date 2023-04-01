2ad5c981bd81f7726d38ac5ffbc4c548bf6404e6
smanmaikano <goreexpress@gmail.com>
2023-04-01 11:07:54 +0100

    final commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,35 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
+    
     public function register()
     {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+        $this->app->singleton(Filesystem::class, function () {
+            return new Filesystem();
+        });
+    
+        $this->app->singleton(Finder::class, function () {
+            return new Finder();
+        });
+    
+        $this->app->singleton(Request::class, function ($app) {
+            return Request::createFromGlobals();
+        });
+    
+        $this->app->singleton(Response::class, function () {
+            return new Response();
+        });
+    
+        $this->app->singleton(HttpKernelInterface::class, function ($app) {
+            return $app->make(\Illuminate\Contracts\Http\Kernel::class);
+        });
     }
 }
